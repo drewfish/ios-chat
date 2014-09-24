89a5ebcfc763e74c4c7f31e0dbde067f8179aabc
Drew Folta <drew@folta.net>
2014-09-23 19:47:33 -0700

    milestone 1

--- chat/AppDelegate.swift
+++ chat/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        Parse.setApplicationId("DXsvTSLgsKT03gSSqy6V5KbLwVpgfEjmEsKzzQUP", clientKey: "BXAzmCJhMtIVWhLVEiKIMzPCA5XI0Nt9NwvAOPVd")
         return true
     }
 
